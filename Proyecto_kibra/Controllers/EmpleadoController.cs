04ceb17a167ad5daef737c5ddcd877dff3f6d2db
José María Guisado Gómez <guigom9@gmail.com>
2016-03-02 14:06:34 +0100

    Version actualizada, empleado Controller, Model..
    Y nuevo también

---
+++ Proyecto_kibra/Controllers/EmpleadoController.cs
@@ -0,0 +1,36 @@
+﻿using Proyecto_kibra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaDal_kibra;
+
+namespace Proyecto_kibra.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        //
+        // GET: /Empleado/
+
+        public ActionResult Nuevo()
+        {
+            EmpleadoModel modelo = new EmpleadoModel();
+
+            ProvinciaDal helperProvincia = new ProvinciaDal();
+            modelo.Provincias = helperProvincia.getListaProvincias();
+
+            PuestoDal helperPuesto = new PuestoDal();
+            modelo.Puestos = helperPuesto.getListaPuestos();
+
+            DepartamentoDal helperDepartamento = new DepartamentoDal();
+            modelo.Departamentos = helperDepartamento.getListaDepartamentos();
+
+            CiudadDal helperCiudad = new CiudadDal();
+            modelo.Ciudades = helperCiudad.getListaCiudades();
+
+            return View(modelo);
+        }
+
+    }
+}
---
+++ Proyecto_kibra/Models/EmpleadoModel.cs
@@ -0,0 +1,23 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_kibra.Models
+{
+    public class EmpleadoModel
+    {
+        public Empleado Empleado { get; set; }
+
+        public List<Provincia> Provincias { get; set; }
+
+        public List<Ciudad> Ciudades { get; set; }
+
+        public List<Puesto> Puestos { get; set; }
+
+        public List<Departamento> Departamentos { get; set; }
+
+        public int ProvinciaId { get; set; }
+    }
+}
