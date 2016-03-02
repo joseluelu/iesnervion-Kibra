1ae1af50febe92027f255cbb728cbd888bf64509
joseluelu <joseluelu@gmail.com>
2016-03-02 14:17:02 +0100

    EmpleadoController

---
+++ Proyecto_kibra/Controllers/EmpleadoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entidades;
+
+namespace Proyecto_kibra.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        //
+        // GET: /Empleado/
+
+        public ActionResult Detalles()
+        {
+            Entidades.Login l = (Entidades.Login)Session["usuario"];
+            Empleado empleado = l.Empleado;
+            return View(empleado);
+        }
+
+    }
+}
